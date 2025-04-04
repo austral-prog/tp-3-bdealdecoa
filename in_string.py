7d9eea63e21cda29afcae81243f07266f2c17a45
bdealdecoa <bdealdecoa@mail.austral.edu.ar>
2025-04-04 12:29:58 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,4 +1,21 @@
 def check_vowels():
+    txt = input("> ") 
+    
+    print("Contiene a:", any(c in txt for c in ("a", "A")))
+    print("Contiene e:", any(c in txt for c in ("e", "E")))
+    print("Contiene i:", any(c in txt for c in ("i", "I")))
+    print("Contiene o:", any(c in txt for c in ("o", "O")))
+    print("Contiene u:", any(c in txt for c in ("u", "U")),"\n")
+    
+    Txt = input("> ") 
+    
+    print("Contiene a:", any(c in Txt for c in ("a", "A")))
+    print("Contiene e:", any(c in Txt for c in ("e", "E")))
+    print("Contiene i:", any(c in Txt for c in ("i", "I")))
+    print("Contiene o:", any(c in Txt for c in ("o", "O")))
+    print("Contiene u:", any(c in Txt for c in ("u", "U")))
+
+
     # Código a implementar utilizando input.
 
 
