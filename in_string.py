3341bd285ea8e66b9b86eb926a432294afe2a0e3
bdealdecoa <bdealdecoa@mail.austral.edu.ar>
2025-04-04 12:47:15 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,23 +1,8 @@
 def check_vowels():
-    txt = input("> ") 
+    txt = input("> ").lower()
     
-    print("Contiene a:", any(c in txt for c in ("a", "A")))
-    print("Contiene e:", any(c in txt for c in ("e", "E")))
-    print("Contiene i:", any(c in txt for c in ("i", "I")))
-    print("Contiene o:", any(c in txt for c in ("o", "O")))
-    print("Contiene u:", any(c in txt for c in ("u", "U")),"\n")
-    
-    Txt = input("> ") 
-    
-    print("Contiene a:", any(c in Txt for c in ("a", "A")))
-    print("Contiene e:", any(c in Txt for c in ("e", "E")))
-    print("Contiene i:", any(c in Txt for c in ("i", "I")))
-    print("Contiene o:", any(c in Txt for c in ("o", "O")))
-    print("Contiene u:", any(c in Txt for c in ("u", "U")))
-
-
-    # Código a implementar utilizando input.
-
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
+    print("Contiene a:", "a" in txt)
+    print("Contiene e:", "e" in txt)
+    print("Contiene i:", "i" in txt)
+    print("Contiene o:", "o" in txt)
+    print("Contiene u:", "u" in txt)
