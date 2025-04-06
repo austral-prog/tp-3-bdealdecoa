6ceffe8443449e4700642e4c58fcfddb3d04a428
bdealdecoa <bdealdecoa@mail.austral.edu.ar>
2025-04-06 17:39:32 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,4 +1,13 @@
 def slice_advanced():
+    texto: "Hello World!"
+    
+    print(texto[2:0:2])
+    
+    numero: "12345678910"
+    
+    print(numero[2:0:2])
+
+slice_advanced
     # Código a implementar utilizando input.
 
 # Para verificar este ejercicio ejecutar el comando
